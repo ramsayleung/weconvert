2550b1ecde294c1ea18dba00b059a9a8212893da
Ramsay Leung <ramsayleung@gmail.com>
2023-12-22 14:44:24 -0800

    Initial Commit

---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by ramsayleung on 2023-12-22.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
